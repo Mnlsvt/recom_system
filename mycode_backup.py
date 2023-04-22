ae0ed130c4eec4cbc1700d515fe51822af04b3ad
MnLsVt <72461719+Mnlsvt@users.noreply.github.com>
2023-04-22 21:20:12 +0300

    Add files via upload

---
+++ mycode_backup.py
@@ -0,0 +1,168 @@
+
+import subprocess
+import re
+
+
+# Code that runs resnet18, resnet50 and alexnet models and gets their outputs in a 3d array called predictions
+
+predictions = [[['' for k in range(3)] for j in range(3)] for i in range(4)]
+
+images = "tzallias.jpg"
+
+with open('unified_code.py', 'r+') as unifiedf:
+        content_unifiedf = unifiedf.readlines()
+        for i, line in enumerate(content_unifiedf):
+            if 'images =' in line:
+                content_unifiedf[i] = 'images = "%s"\n' %images  # Modify the line with the new value
+                break  # Stop searching for the line once found
+        unifiedf.seek(0)  # Go back to the beginning of the file
+        unifiedf.writelines(content_unifiedf)  # Write the modified content back to the file
+        unifiedf.truncate()
+
+for k in range(3):
+    if (k == 0):
+        arch = "resnet18"
+    elif (k == 1):
+        arch = "resnet50"
+    else:
+        arch = "alexnet"
+    with open('basic_code.py', 'r+') as file:
+        content = file.readlines()
+        for i, line in enumerate(content):
+            if 'arch =' in line:
+                content[i] = 'arch = "%s"\n' %arch  # Modify the line with the new value
+            elif 'images =' in line:
+                content[i] = 'images = "%s"\n' %images  # Modify the line with the new value
+                break  # Stop searching for the line once found
+        file.seek(0)  # Go back to the beginning of the file
+        file.writelines(content)  # Write the modified content back to the file
+
+
+
+    # run basic_code.py and capture its output
+    result = subprocess.run(['python3', 'basic_code.py'], stdout=subprocess.PIPE)
+
+    # get the output of script1.py as input
+    input_data = result.stdout.decode('utf-8').strip()
+
+    # do something with the input data
+    #predictions = [['',''],['',''],['','']]
+
+    for i in range(3):
+        pred_lines = input_data.split('\n')[i+1]
+        # find the accuracy of each possible prediction
+        pred_acc = pred_lines[2:5]
+
+        # keep a string of the possible prediction's name
+        pred1 = ""
+        space_count = 0
+        for j in pred_lines:
+            if (space_count == 1):
+                if (j != " "):
+                    pred1 += j
+            if (j == ">"):
+                space_count = 1
+        space_count = 0
+        if (int(pred_acc) >= 200):
+            predictions[k][i][0] = arch
+            predictions[k][i][1] = pred_acc
+            predictions[k][i][2] = pred1
+        else:
+            predictions[k][i][0] = "none"
+            predictions[k][i][1] = "none"
+            predictions[k][i][2] = "none"
+        #print(pred_acc, pred1)
+#print(predictions)
+
+
+'''
+predictions is a 3d array in which the values are assigned like this:
+
+[
+    3x arches
+    [
+        3x predictions
+        [arch1, acc1, prediction_name1]
+        [arch1, acc2, prediction_name2]
+        [arch1, acc3, prediction_name3]
+        .
+        .
+        .
+    ]
+    .
+    .
+    .
+]
+
+'''
+
+# Code that runs the wideresnet model and gets the image attributes as well
+
+attribute_predictions = []
+
+# run basic_code.py and capture its output
+result = subprocess.run(['python3', 'unified_code.py'], stdout=subprocess.PIPE)
+
+# get the output of script1.py as input
+input_data = result.stdout.decode('utf-8').strip()
+arch = "widerestnet"
+
+# do something with the input data
+
+for i in range(3):
+    pred_lines = input_data.split('\n')[i+2]
+    
+    # find the accuracy of each possible prediction
+    pred_acc = pred_lines[2:5]
+
+    # keep a string of the possible prediction's name
+    pred1 = ""
+    space_count = 0
+    for j in pred_lines:
+        if (space_count == 1):
+            if (j != " "):
+                pred1 += j
+        if (j == ">"):
+            space_count = 1
+    space_count = 0
+    if (int(pred_acc) >= 200):
+        predictions[3][i][0] = arch
+        predictions[3][i][1] = pred_acc
+        predictions[3][i][2] = pred1
+    else:
+        predictions[k][i][0] = "none"
+        predictions[k][i][1] = "none"
+        predictions[k][i][2] = "none"
+    #print(pred_acc, pred1)
+pred_lines_attr = input_data.split('\n')
+pred_attr = pred_lines_attr[-2]
+pred_attr += ","
+print(pred_attr)
+
+#for j in range(9): # 9 is the number of the attributes that we take from the widerestnet model
+temp_word = ""
+word_counter = 0
+for n in (pred_attr):
+    if (n != ","):
+        temp_word += n
+    else:
+        if (temp_word[0] == " "):
+            temp_word = temp_word[1:]
+        attribute_predictions.append(temp_word)
+        temp_word = ""
+        word_counter += 1
+
+predictions = [[cell for cell in row if 'none' not in cell] for row in predictions if any('none' not in cell for cell in row)]
+predictions = [[cell for cell in row if '' not in cell] for row in predictions if any('' not in cell for cell in row)]
+
+if len(predictions) > 0:
+    print(predictions[0],"\n")
+if len(predictions) > 1:
+    print(predictions[1],"\n")
+if len(predictions) > 2:
+    print(predictions[2],"\n")
+if len(predictions) > 3:
+    print(predictions[3],"\n")
+
+print(attribute_predictions)
+
