cdcbdaa0c60b0df6c41000616cf74ce72b7426e7
mnlsvt <manolis277@gmail.com>
2023-06-11 22:40:23 +0300

    fixed the redirect 404 problem with the urls

--- Pint_UI/src/App.js
+++ Pint_UI/src/App.js
@@ -26,14 +26,6 @@ const db = firebase.firestore();
 const storage = firebase.storage();
 
 
-const isSmallScreen = () => {
-    return window.innerWidth <= 768;
-};
-
-const isTouchscreenDevice = () => {
-    return 'ontouchstart' in window || navigator.maxTouchPoints;
-};
-
 
 function SignInWithGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider();
@@ -108,8 +100,8 @@ function Upload({ user }) {
         const fileRef = storageRef.child(file.name);
         await fileRef.put(file);
         const fileUrl = await fileRef.getDownloadURL();
-        setRecentImageUrls(prevUrls => [fileUrl, ...prevUrls]);
-
+        setRecentImageUrls(prevUrls => [fileUrl, ...prevUrls]); 
+    
         await db.collection('images').add({
             url: fileUrl,
             uploaderId: user.uid,
@@ -118,7 +110,7 @@ function Upload({ user }) {
         setIsLoading(false);
         setFile(null);
     };
-
+    
 
     return (
         <div>
@@ -134,8 +126,8 @@ function Upload({ user }) {
             </div>
         </div>
     );
-
-
+    
+    
 }
 
 
@@ -167,7 +159,7 @@ function App() {
 
         return () => unsubscribe();
     }, []);
-
+    
 
     const handleLike = async (id) => {
         const docRef = db.collection('images').doc(id);
@@ -191,18 +183,17 @@ function App() {
                 ? { ...prevSelectedImage, likes: likes.includes(user.uid) ? likes.filter(uid => uid !== user.uid) : [...likes, user.uid] }
                 : prevSelectedImage
         );
-
     };
 
     const handleDelete = async (id, url) => {
         const docRef = db.collection('images').doc(id);
         await docRef.delete();
-
+    
         const imageRef = storage.refFromURL(url);
         await imageRef.delete();
-
+    
         setImages(prevImages => prevImages.filter(image => image.id !== id));
-    };
+    };    
 
 
     const handleDoubleLike = (id) => {
@@ -212,120 +203,109 @@ function App() {
             setShowHeart(true);
             setTimeout(() => setShowHeart(false), 3000);
         }
-
-        // If screen is larger than 768px, return early to prevent liking
-        if (!isSmallScreen()) {
-            return;
-        }
-
-        if (!selectedLikes.includes(user.uid)) {
-            handleLike(id);
-            setShowHeart(true);
-            setTimeout(() => setShowHeart(false), 3000);
-        }
-};
+    };
 
 
 
-const handleSelect = (image) => {
-    setSelectedImage(image);
-    setExpanded(true);
-};
+    const handleSelect = (image) => {
+        setSelectedImage(image);
+        setExpanded(true);
+    };
 
-const handleDeselect = () => {
-    setSelectedImage(null);
-    setExpanded(false);
-};
+    const handleDeselect = () => {
+        setSelectedImage(null);
+        setExpanded(false);
+    };
 
-const handleSignOut = () => {
-    auth.signOut();
-};
+    const handleSignOut = () => {
+        auth.signOut();
+    };
 
-return (
-    <Router>
-        <div className="App">
-            <header className="App-header">
-                <h1>Ptuxiakh Manwlh</h1>
-                {user && (
-                    <div className="user-info">
-                        <img src={user.photoURL} alt="User" />
-                        <Link to="/profile">{user.displayName}</Link>
-                        <Link to="/upload">Upload</Link>
-                        <button className="logout-button" onClick={handleSignOut}>Logout</button>
-                    </div>
-                )}
-            </header>
+    return (
+        <Router>
+            <div className="App">
+                <header className="App-header">
+                    <h1>Ptuxiakh Manwlh</h1>
+                    {user && (
+                        <div className="user-info">
+                            <img src={user.photoURL} alt="User" />
+                            <Link to="/profile">{user.displayName}</Link>
+                            <Link to="/upload">Upload</Link>
+                            <button className="logout-button" onClick={handleSignOut}>Logout</button>
+                        </div>
+                    )}
+                </header>
 
-            <Routes>
-                <Route path="/profile" element={<Profile user={user} images={images} onDelete={handleDelete} />} />
-                <Route path="/upload" element={<Upload user={user} />} />
-                <Route path="/" element={
-                    user ? (
-                        <div className="masonry">
-                            {images.map(image => (
-                                <div key={image.id} className="image-item" onClick={() => handleSelect(image)}>
-                                    <img src={image.url} alt="" />
-                                    <div className="image-item-info">
-                                        <button onClick={(e) => { e.stopPropagation(); handleLike(image.id); }}>
-                                            {image.likes && image.likes.includes(user.uid) ? "❤️" : "🤍"}
-                                        </button>
-                                    </div>
-                                </div>
-                            ))}
-                            {selectedImage && isExpanded && (
-                                <div className="expandedImage" onClick={(e) => e.target === e.currentTarget && handleDeselect()}>
-                                    <div className="expandedImageContainer">
-                                        <img
-                                            src={selectedImage.url}
-                                            alt=""
-                                            onDoubleClick={(e) => {
-                                                e.stopPropagation();
-                                                handleDoubleLike(selectedImage.id);
-                                            }}
-                                        />
-                                        {showHeart && <div className="heart">❤️</div>}
+                <Routes>
+                    <Route path="/profile" element={<Profile user={user} images={images} onDelete={handleDelete} />} />
+                    <Route path="/upload" element={<Upload user={user} />} />
+                    <Route path="/" element={
+                        user ? (
+                            <div className="masonry">
+                                {images.map(image => (
+                                    <div key={image.id} className="image-item" onClick={() => handleSelect(image)}>
+                                        <img src={image.url} alt="" />
                                         <div className="image-item-info">
-                                            <button
-                                                onClick={(e) => {
-                                                    e.stopPropagation();
-                                                    handleLike(selectedImage.id);
-                                                }}
-                                            >
-                                                {selectedImage.likes && selectedImage.likes.includes(user.uid) ? "❤️" : "🤍"}
+                                            <button onClick={(e) => { e.stopPropagation(); handleLike(image.id); }}>
+                                                {image.likes && image.likes.includes(user.uid) ? "❤️" : "🤍"}
                                             </button>
                                         </div>
                                     </div>
-                                </div>
-                            )}
-                        </div>
-                    ) : (
-                        <div className="login-form">
-                            <h2>Sign In</h2>
-                            <button onClick={SignInWithGoogle}>Sign in with Google</button>
-                            <form onSubmit={(e) => {
-                                e.preventDefault();
-                                SignInWithEmailPassword(e.target.email.value, e.target.password.value);
-                            }}>
-                                <input name="email" type="email" placeholder="Email" />
-                                <input name="password" type="password" placeholder="Password" />
-                                <button type="submit">Sign in</button>
-                            </form>
-                            <h2>Sign Up</h2>
-                            <form onSubmit={(e) => {
-                                e.preventDefault();
-                                SignUpWithEmailPassword(e.target.email.value, e.target.password.value);
-                            }}>
-                                <input name="email" type="email" placeholder="Email" />
-                                <input name="password" type="password" placeholder="Password" />
-                                <button type="submit">Sign up</button>
-                            </form>
-                        </div>
-                    )
-                } />
-            </Routes>
-        </div>
-    </Router>
-);
+                                ))}
+                                {selectedImage && isExpanded && (
+                                    <div className="expandedImage" onClick={(e) => e.target === e.currentTarget && handleDeselect()}>
+                                        <div className="expandedImageContainer">
+                                            <img
+                                                src={selectedImage.url}
+                                                alt=""
+                                                onDoubleClick={(e) => {
+                                                    e.stopPropagation();
+                                                    handleDoubleLike(selectedImage.id);
+                                                }}
+                                            />
+                                            {showHeart && <div className="heart">❤️</div>}
+                                            <div className="image-item-info">
+                                                <button
+                                                    onClick={(e) => {
+                                                        e.stopPropagation();
+                                                        handleLike(selectedImage.id);
+                                                    }}
+                                                >
+                                                    {selectedImage.likes && selectedImage.likes.includes(user.uid) ? "❤️" : "🤍"}
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )}
+                            </div>
+                        ) : (
+                            <div className="login-form">
+                                <h2>Sign In</h2>
+                                <button onClick={SignInWithGoogle}>Sign in with Google</button>
+                                <form onSubmit={(e) => {
+                                    e.preventDefault();
+                                    SignInWithEmailPassword(e.target.email.value, e.target.password.value);
+                                }}>
+                                    <input name="email" type="email" placeholder="Email" />
+                                    <input name="password" type="password" placeholder="Password" />
+                                    <button type="submit">Sign in</button>
+                                </form>
+                                <h2>Sign Up</h2>
+                                <form onSubmit={(e) => {
+                                    e.preventDefault();
+                                    SignUpWithEmailPassword(e.target.email.value, e.target.password.value);
+                                }}>
+                                    <input name="email" type="email" placeholder="Email" />
+                                    <input name="password" type="password" placeholder="Password" />
+                                    <button type="submit">Sign up</button>
+                                </form>
+                            </div>
+                        )
+                    } />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 
